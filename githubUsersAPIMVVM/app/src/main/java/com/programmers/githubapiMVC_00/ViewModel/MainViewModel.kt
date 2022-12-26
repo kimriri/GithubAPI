834b61651d0eeff29dd9957492663a0cd33dddba
eunmikim <eunmikim@fatoscorp.com>
2022-12-26 18:44:53 +0900

    gitignore test

--- githubUsersAPIMVVM/app/src/main/java/com/programmers/githubapiMVC_00/ViewModel/MainViewModel.kt
+++ githubUsersAPIMVVM/app/src/main/java/com/programmers/githubapiMVC_00/ViewModel/MainViewModel.kt
@@ -1,14 +1,9 @@
 package com.programmers.githubapiMVC_00.ViewModel
 
-import android.util.Log
 import androidx.lifecycle.*
-import com.programmers.githubapiMVC_00.Model.GithubService.Data.UserResponse
 import com.programmers.githubapiMVC_00.Model.GithubService.Data.UsersData
-import com.programmers.githubapiMVC_00.Model.GithubService.UsersService.UsersServiceManager
-import com.programmers.githubapiMVC_00.View.UsersAdapter
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Callback
+
 
 
 class MainViewModel: ViewModel() {
