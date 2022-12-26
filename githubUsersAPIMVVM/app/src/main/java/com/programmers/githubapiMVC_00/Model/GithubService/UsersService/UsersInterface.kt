cf5c69ba686fd4db6ad44f0e2224efb847549860
eunmikim <eunmikim@fatoscorp.com>
2022-12-26 18:47:19 +0900

    gitignore test

--- githubUsersAPIMVVM/app/src/main/java/com/programmers/githubapiMVC_00/Model/GithubService/UsersService/UsersInterface.kt
+++ githubUsersAPIMVVM/app/src/main/java/com/programmers/githubapiMVC_00/Model/GithubService/UsersService/UsersInterface.kt
@@ -9,7 +9,7 @@ import retrofit2.http.Query
 
 interface UsersInterface {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUsers(@Query("q") query: String)
     :  retrofit2.Call<UserResponse>
 
