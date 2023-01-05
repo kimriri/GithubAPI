2ff5d7eb9d5ed032e0ce4969a492dbf94b0f5747
eunmikim <eunmikim@fatoscorp.com>
2023-01-05 14:07:26 +0900

    viewModelScope,observe

--- githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/data/viewmodel/MainViewModel.kt
+++ githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/data/viewmodel/MainViewModel.kt
@@ -1,9 +1,13 @@
 package com.programmers.githubapimvvm.data.viewmodel
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import androidx.lifecycle.*
 import com.programmers.githubapimvvm.data.repository.UserResponse
 import com.programmers.githubapimvvm.data.repository.UsersData
 import com.programmers.githubapimvvm.data.viewmodel.retrofitapi.UsersServiceManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
@@ -13,13 +17,19 @@ class MainViewModel: ViewModel() {
     val _liveData = MutableLiveData<MutableList<UsersData>?>()
     lateinit var _liveSearch : String
 
-    fun saveData(userList: MutableList<UsersData>) = viewModelScope.launch{
+    fun saveData(userList: MutableList<UsersData>) {
         _liveData.value = userList
         _liveData.postValue(userList)
     }
 
-    fun saveSearch(liveSearch: String) {
-        _liveSearch = liveSearch
+    fun saveSearch(livesearch: String) = viewModelScope.launch {
+        _liveSearch = livesearch
+            val rqUserList = resultSearch()
+            if(rqUserList.isSuccessful) {
+                saveData(rqUserList.body()?.items!!)
+            }else {
+                Log.e(TAG,rqUserList.message())
+            }
     }
 
     suspend fun resultSearch(): Response<UserResponse> {
--- githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/BindingAdapter/ImageViewBindingAdapter.kt
+++ githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/BindingAdapter/ImageViewBindingAdapter.kt
@@ -1,12 +1,17 @@
 package com.programmers.githubapimvvm.data.viewmodel
 
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.programmers.githubapimvvm.data.repository.UsersData
+import com.programmers.githubapimvvm.view.UsersAdapter
 
 
-    @BindingAdapter("profileImage")
+@BindingAdapter("profileImage")
     fun ImageView.profileImage(imageUrl: String) {
         Glide.with(this)
             .load(imageUrl)
