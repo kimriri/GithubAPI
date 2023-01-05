f3f0a33ad675b7a229a3e5b0e4c2a45367d4a1bc
eunmikim <eunmikim@fatoscorp.com>
2023-01-05 14:07:26 +0900

    viewModelScope,observe

--- githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/MainActivity.kt
+++ githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/MainActivity.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.lifecycle.Observer
 import com.programmers.githubapimvvm.R
 import com.programmers.githubapimvvm.data.viewmodel.MainViewModel
 import com.programmers.githubapimvvm.databinding.ActivityMainBinding
@@ -16,32 +16,24 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity()  {
 
-
     private lateinit var binding: ActivityMainBinding
     private val viewmodel : MainViewModel by viewModels()
-    private val listAdapter: UsersAdapter = UsersAdapter()
     val TAG by lazy {  this.componentName.className}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.search = this
-        binding.rvMain.apply {
-            adapter = listAdapter
-            layoutManager = LinearLayoutManager(context)
-            setHasFixedSize(true)
-        }
+
+        binding.rvMain.adapter = UsersAdapter(emptyList())
     }
     fun searchEvent() {
          viewmodel.saveSearch(binding.etMain.text.toString())
-         CoroutineScope(Dispatchers.Main).launch {
-             val rqUserList = viewmodel.resultSearch()
-             if(rqUserList.isSuccessful) {
-                 Log.d(TAG,rqUserList.code().toString())
-                 viewmodel.saveData(rqUserList.body()?.items!!)
-                 listAdapter.setList(viewmodel._liveData)
-             }else {
-                 Log.e(TAG,rqUserList.message())
-             }
-         }
+         viewmodel._liveData.observe(this, Observer {
+            (binding.rvMain.adapter as UsersAdapter).update(it!!)
+
+        })
+
+
+
     }
 }
--- githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/UsersAdapter.kt
+++ githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapimvvm/view/UsersAdapter.kt
@@ -1,40 +1,45 @@
 package com.programmers.githubapimvvm.view
 
+import android.renderscript.ScriptGroup.Binding
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.MutableLiveData
 import androidx.recyclerview.widget.RecyclerView
 import com.programmers.githubapimvvm.data.repository.UsersData
-import com.programmers.githubapimvvm.data.viewmodel.MainViewModel
 import com.programmers.githubapimvvm.databinding.UserItemBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import okhttp3.internal.notifyAll
 
 
-class UsersAdapter: RecyclerView.Adapter<UsersAdapter.MyView>() {
-    private var userList = mutableListOf<UsersData>()
-    inner class MyView(private val binding: UserItemBinding): RecyclerView.ViewHolder(binding.root) {
+
+class UsersAdapter(private val context: List<Any>) : RecyclerView.Adapter<UsersAdapter.MyView>() {
+     var userList = mutableListOf<UsersData>()
+
+    private lateinit var binding: UserItemBinding
+
+    inner class MyView(binding: UserItemBinding): RecyclerView.ViewHolder(binding.root) {
         fun bind(currentUser: UsersData) {
-            this.binding.users = currentUser
+            binding.apply {
+                binding.users = currentUser
+            }
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : MyView {
-         val binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        binding = UserItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MyView(binding)
     }
 
+
     override fun onBindViewHolder(holder: MyView, position: Int) {
         holder.bind(userList[position])
+         binding.executePendingBindings()
     }
 
     override fun getItemCount(): Int {
         return userList.size
     }
 
-    fun setList(list: MutableLiveData<MutableList<UsersData>?>) {
-        userList = list.value!!
+    fun update(list: MutableList<UsersData>) {
+        userList = list
         notifyDataSetChanged()
+
     }
 }
 
