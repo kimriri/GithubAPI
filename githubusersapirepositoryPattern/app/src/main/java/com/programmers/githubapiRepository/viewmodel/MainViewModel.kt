f48b2e90d0fe43b0eecf52b917cbe60fa6e840be
kimriri <dmsal547@gmail.com>
2023-01-07 11:10:17 +0900

    add  Toast message when Error

--- githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapiRepository/viewmodel/MainViewModel.kt
+++ githubusersapirepositoryPattern/app/src/main/java/com/programmers/githubapiRepository/viewmodel/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.programmers.githubapiRepository.viewmodel
 
 import android.content.ContentValues.TAG
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.*
 import com.programmers.githubapiRepository.data.UserResponse
 import com.programmers.githubapiRepository.data.UsersData
@@ -14,18 +15,28 @@ class MainViewModel: ViewModel() {
 
     val _liveData = MutableLiveData<MutableList<UsersData>?>()
     lateinit var _liveSearch : String
+    var _rquserList : String = "userList "
 
     fun saveData(userList: MutableList<UsersData>) {
         _liveData.value = userList
         _liveData.postValue(userList)
     }
 
+    private fun rquserList(rqdata: String) {
+        _rquserList = rqdata
+    }
+    private fun errorMessage(rqdata: String) {
+        _rquserList = rqdata
+    }
     fun saveSearch(livesearch: String) = viewModelScope.launch {
         _liveSearch = livesearch
             val rqUserList = resultSearch()
-            if(rqUserList.isSuccessful) {
+            if(rqUserList.isSuccessful)  {
                 saveData(rqUserList.body()?.items!!)
+                rquserList("Successful")
             }else {
+                rquserList("ERROR")
+                errorMessage(rqUserList.message())
                 Log.e(TAG,rqUserList.message())
             }
     }
